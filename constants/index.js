158e4c9989045ac61afb91c28f637decde95c16a
PranavKohli24 <kohlipranav24@gmail.com>
2025-06-24 16:49:50 +0530

    final code deploy

--- constants/index.js
+++ constants/index.js
@@ -110,27 +110,27 @@ const navLinks = [
    };
    
    const openingHours = [
-    { day: "Mon–Thu", time: "11:00am – 12am" },
+    { day: "Mon–Thu", time: "11:00am – 11:00pm" },
     { day: "Fri", time: "11:00am – 2am" },
     { day: "Sat", time: "9:00am – 2am" },
     { day: "Sun", time: "9:00am – 1am" },
    ];
    
    const socials = [
-    {
+      {
        name: "Instagram",
        icon: "/images/insta.png",
-       url: "#",
+       url: "https://www.linkedin.com/in/pranav-kohli-987aa5226/",
     },
     {
        name: "X (Twitter)",
        icon: "/images/x.png",
-       url: "#",
+       url: "https://www.linkedin.com/in/pranav-kohli-987aa5226/",
     },
     {
        name: "Facebook",
        icon: "/images/fb.png",
-       url: "#",
+       url: "https://www.linkedin.com/in/pranav-kohli-987aa5226/",
     }
     
    ];
